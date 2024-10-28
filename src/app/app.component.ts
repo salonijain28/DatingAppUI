2de7f4cea64e1e928ad6513df3434d9d26f03f23
Saloni jain <salonijain2801@gmail.com>
2024-10-28 17:46:46 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+  constructor(private httpClient : HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:58620/api/users').subscribe({
+      next : response => this.users = response,
+      error : error => console.log(error),
+      complete : () => console.log ("Request has been complete")
+    })
+  }
 }
